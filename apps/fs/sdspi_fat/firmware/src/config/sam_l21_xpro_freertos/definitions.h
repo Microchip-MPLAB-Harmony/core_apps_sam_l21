32b5d582eeaef88bbeb2acc7db356d1c80c69399
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-02 11:13:32 +0530

    [SYS_FS] Regenerated demo for fs_sdspi_fat_sam_l21_xpro_freertos
    MH3-50733
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/sdspi_fat/firmware/src/config/sam_l21_xpro_freertos/definitions.h
+++ apps/fs/sdspi_fat/firmware/src/config/sam_l21_xpro_freertos/definitions.h
@@ -66,12 +66,12 @@
 #include "system/fs/fat_fs/hardware_access/diskio.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "bsp/bsp.h"
 #include "app.h"
 
 
@@ -83,6 +83,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 48000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -176,22 +179,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* SDSPI0 Driver Object */
