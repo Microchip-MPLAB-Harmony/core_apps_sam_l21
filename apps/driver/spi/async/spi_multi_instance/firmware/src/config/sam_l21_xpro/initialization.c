301c40c142afb3088d50238e95162555d48b43fd
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-17 12:32:24 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_multi_instance_sam_l21_xpro
    MH3-56644
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_l21_xpro/initialization.c
+++ apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_l21_xpro/initialization.c
@@ -94,7 +94,7 @@ const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
     .writeRead = (DRV_SPI_PLIB_WRITE_READ)SERCOM0_SPI_WriteRead,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SPI_PLIB_IS_BUSY)SERCOM0_SPI_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SERCOM0_SPI_IsTransmitterBusy,
 
     /* SPI PLIB Callback Register */
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SERCOM0_SPI_CallbackRegister,
@@ -131,11 +131,6 @@ const DRV_SPI_INIT drvSPI0InitData =
     /* SPI Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvSPI0ClientObjPool[0],
 
-    /* DMA Channel for Transmit */
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    /* DMA Channel for Receive */
-    .dmaChannelReceive  = SYS_DMA_CHANNEL_NONE,
 
     /* SPI Queue Size */
     .transferObjPoolSize = DRV_SPI_QUEUE_SIZE_IDX0,
@@ -166,7 +161,7 @@ const DRV_SPI_PLIB_INTERFACE drvSPI1PlibAPI = {
     .writeRead = (DRV_SPI_PLIB_WRITE_READ)SERCOM5_SPI_WriteRead,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SPI_PLIB_IS_BUSY)SERCOM5_SPI_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SERCOM5_SPI_IsTransmitterBusy,
 
     /* SPI PLIB Callback Register */
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SERCOM5_SPI_CallbackRegister,
@@ -203,11 +198,6 @@ const DRV_SPI_INIT drvSPI1InitData =
     /* SPI Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvSPI1ClientObjPool[0],
 
-    /* DMA Channel for Transmit */
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    /* DMA Channel for Receive */
-    .dmaChannelReceive  = SYS_DMA_CHANNEL_NONE,
 
     /* SPI Queue Size */
     .transferObjPoolSize = DRV_SPI_QUEUE_SIZE_IDX1,
@@ -266,7 +256,7 @@ SYSTEM_OBJECTS sysObj;
 void SYS_Initialize ( void* data )
 {
 
-    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3UL);
 
     PM_Initialize();
 
--- apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
+++ apps/driver/spi/async/spi_multi_instance/firmware/src/config/sam_l21_xpro/peripheral/port/plib_port.c
@@ -76,8 +76,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_OUT = 0x121024;
    PORT_REGS->GROUP[0].PORT_PINCFG[2] = 0x6;
    PORT_REGS->GROUP[0].PORT_PINCFG[4] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[5] = 0x0;
    PORT_REGS->GROUP[0].PORT_PINCFG[6] = 0x1;
    PORT_REGS->GROUP[0].PORT_PINCFG[7] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[12] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PINCFG[20] = 0x0;
 
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x3;
    PORT_REGS->GROUP[0].PORT_PMUX[3] = 0x33;
@@ -86,6 +90,9 @@ void PORT_Initialize(void)
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x1440;
    PORT_REGS->GROUP[1].PORT_OUT = 0x1440;
+   PORT_REGS->GROUP[1].PORT_PINCFG[6] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PINCFG[12] = 0x0;
    PORT_REGS->GROUP[1].PORT_PINCFG[16] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[22] = 0x1;
    PORT_REGS->GROUP[1].PORT_PINCFG[23] = 0x1;
@@ -304,7 +311,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -315,7 +322,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
